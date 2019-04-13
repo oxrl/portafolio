00b2dbe7fd3e9ef4e32002439daab9bd7621bc0b
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2019-04-13 18:52:18 -0500

    Se agrego funcionalidad para el mantenimiento de experiencias y esutdios en el BackOffice

--- Model/Experiencia.cs
+++ Model/Experiencia.cs
@@ -4,7 +4,9 @@ namespace Model
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Experiencia")]
     public partial class Experiencia
@@ -35,5 +37,140 @@ namespace Model
         public string Descripcion { get; set; }
 
         public virtual Usuario Usuario { get; set; }
+
+        public Experiencia Obtener(int id)
+        {
+            var experiencia = new Experiencia();
+
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    experiencia = ctx.Experiencia.Where(x => x.id == id)
+                                                 .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return experiencia;
+        }
+
+        public ResponseModel Guardar()
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    if (this.id > 0) ctx.Entry(this).State = EntityState.Modified;
+                    else ctx.Entry(this).State = EntityState.Added;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        public ResponseModel Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    this.id = id;
+                    ctx.Entry(this).State = EntityState.Deleted;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        public AnexGRIDResponde Listar(AnexGRID grid, int tipo)
+        {
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+
+                    grid.Inicializar();
+
+                    var query = ctx.Experiencia.Where(x => x.Tipo == tipo);
+
+                    // Ordenamiento
+                    if (grid.columna == "id")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.id)
+                                                             : query.OrderBy(x => x.id);
+                    }
+
+                    if (grid.columna == "Nombre")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Nombre)
+                                                             : query.OrderBy(x => x.Nombre);
+                    }
+
+                    if (grid.columna == "Titulo")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Titulo)
+                                                             : query.OrderBy(x => x.Titulo);
+                    }
+
+                    if (grid.columna == "Desde")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Desde)
+                                                             : query.OrderBy(x => x.Desde);
+                    }
+
+                    if (grid.columna == "Hasta")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Hasta)
+                                                             : query.OrderBy(x => x.id);
+                    }
+
+                    // id, Nombre, Titulo, Desde, Hasta
+
+                    var experiencias = query.Skip(grid.pagina)
+                                            .Take(grid.limite)
+                                             .ToList();
+
+                    var total = query.Count();
+
+                    grid.SetData(experiencias, total);
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return grid.responde();
+        }
+
+
+
     }
 }
--- proyecto/Areas/Admin/Controllers/ExperienciaController.cs
+++ proyecto/Areas/Admin/Controllers/ExperienciaController.cs
@@ -1,4 +1,6 @@
-﻿using proyecto.Areas.Admin.Filters;
+﻿using Helper;
+using Model;
+using proyecto.Areas.Admin.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,15 +9,56 @@ using System.Web.Mvc;
 
 namespace proyecto.Areas.Admin.Controllers
 {
-    [Autenticado]
     public class ExperienciaController : Controller
     {
-        // GET: Admin/Experiencia
-        // GET: Admin/Experiencia
-        public ActionResult Index(int tipo)
+        private Experiencia experiencia = new Experiencia();
+
+        public ActionResult Index(int tipo = 1)
         {
+            ViewBag.tipo = tipo;
             ViewBag.Title = tipo == 1 ? "Trabajos realizados" : "Estudios previos";
             return View();
+        }
+
+        public JsonResult Listar(AnexGRID grid, int tipo)
+        {
+            return Json(experiencia.Listar(grid, tipo));
+        }
+
+        public ActionResult Crud(byte tipo = 0, int id = 0)
+        {
+            if (id == 0)
+            {
+                if (tipo == 0) return Redirect("~/admin/experiencia");
+
+                experiencia.Tipo = tipo;
+                experiencia.Usuario_id = SessionHelper.GetUser();
+            }
+            else experiencia = experiencia.Obtener(id);
+
+            return View(experiencia);
+        }
+
+        public JsonResult Guardar(Experiencia model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/admin/experiencia/?tipo=" + model.Tipo);
+                }
+            }
+
+            return Json(rm);
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            return Json(experiencia.Eliminar(id));
         }
     }
 }
